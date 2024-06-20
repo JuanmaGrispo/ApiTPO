175ba7e29b7b3d0a3d64f1493bc4d0caee83690c
JuanmaGrispo <145155051+JuanmaGrispo@users.noreply.github.com>
2024-06-20 13:30:27 -0300

    first commit

---
+++ src/main/java/com/apiTPO/technologyHouse/TechnologyHouseApplication.java
@@ -0,0 +1,13 @@
+package com.apiTPO.technologyHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiTPO/technologyHouse/TechnologyHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiTPO.technologyHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
